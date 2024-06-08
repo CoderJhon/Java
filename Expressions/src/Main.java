0fe7405996b979c49b664546d8596289dc95d4b2
Jhon <136621282+CoderJhon@users.noreply.github.com>
2024-06-08 12:58:42 -0400

    Add files via upload

---
+++ Expressions/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		// expression = operands and operators
+		// operands = values, variables, numbers, quantity
+		//operators = + - * / %
+		// friends++ will add 1 to the number
+		//friends-- will subtract 1 to the number
+		
+		
+		double friends = 10;
+		
+		friends = (double) friends / 3;
+		
+		System.out.println(friends);
+	}
+
+}
