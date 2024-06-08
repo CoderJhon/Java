6559b1ea567adf9dd033ce795d4da69c06a2e683
Jhon <136621282+CoderJhon@users.noreply.github.com>
2024-06-08 18:26:31 -0400

    Add files via upload

---
+++ Strings/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+
+		String name = "Jhon";
+		
+		//boolean result = name.equalsIgnoreCase("Jhon");
+		
+		//int result = name.length();
+		
+		//char result = name.charAt(0);
+		
+		//int result = name.indexOf("n");
+		
+		//boolean result = name.isEmpty();
+		
+		//String result = name.toUpperCase();
+		
+		//String result = name.toLowerCase();
+		
+		//String result = name.trim();
+		
+		String result = name.replace('o', 'a');
+		
+		System.out.println(result);
+		
+	}
+
+}
