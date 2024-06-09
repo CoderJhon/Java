c79d5db8611813170f3081db96a6ebd4c1b59684
Jhon <136621282+CoderJhon@users.noreply.github.com>
2024-06-09 00:27:50 -0400

    Add files via upload

---
+++ ToString/src/Car.java
@@ -0,0 +1,18 @@
+
+public class Car 
+{
+	String make = "Ford";
+	String model = "Mustang";
+	String color = "Black";
+	int year = 2013;
+	
+	public String toString() 
+	{
+		
+		String myString = make + "\n" + model + "\n" + color + "\n" + year;
+		
+		
+		return myString;
+	}
+	
+}
---
+++ ToString/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		// toString() = special method that all objects inherit,
+		//				that  return a string that "textually represents" an object.
+		//				can be used both implicitly and explicitly
+				
+		Car car = new Car();
+		
+		
+		//System.out.println(car.make);
+		//Sy1stem.out.println(car.model);
+		//System.out.println(car.color);
+		//System.out.println(car.year);
+		
+		//System.out.println(car.toString());
+		System.out.println(car);
+		
+	}
+
+}
