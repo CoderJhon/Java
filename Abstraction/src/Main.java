b72f3f993b6355176c2061e9f07897bc576279a5
Jhon <136621282+CoderJhon@users.noreply.github.com>
2024-06-09 11:41:25 -0400

    Add files via upload

---
+++ Abstraction/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		//abstract = abstract classes cannot be instantiated, but they can have a subclass
+		//			 abstract methods are delcared without an implementation
+		
+		//Vehicle vehicle = new Vehicle();
+		
+		Car car = new Car();
+		
+	}
+
+}
