1f441a4e4f8163d809c2155daae9639995220dac
Jhon <136621282+CoderJhon@users.noreply.github.com>
2024-06-08 23:27:50 -0400

    Add files via upload

---
+++ FinalKeyWord/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		// you cannot update/changed a final 
+		final double PI = 3.14159;
+		
+		
+		System.out.println(PI);
+	}
+
+}
