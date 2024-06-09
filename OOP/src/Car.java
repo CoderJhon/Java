cdbc4696ee5019cb17e0378ede55da4c2326a932
Jhon <136621282+CoderJhon@users.noreply.github.com>
2024-06-08 23:41:10 -0400

    Add files via upload

---
+++ OOP/src/Car.java
@@ -0,0 +1,19 @@
+
+public class Car 
+{	
+	String make = "Ford";
+	String model = "Mustang";
+	int year = 2013;
+	String color = "black";
+	double price = 50000.00;
+
+	void drive()
+	{
+		System.out.println("You drive the car");
+		
+	}
+	void brake() 
+	{
+		System.out.println("You hit the brakes");
+	}
+}
---
+++ OOP/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		//object = an instance of a class that may contain attributes and methods
+		// example: (phones, cups, desk, computer)
+		
+		Car myCar1 = new Car();
+		Car myCar2 = new Car();
+		
+		System.out.println(myCar1.make);
+		System.out.println(myCar1.model);
+		System.out.println();
+		System.out.println(myCar2.make);
+		System.out.println(myCar2.model);
+		
+		//myCar.drive();
+		//myCar1.brake();
+	}
+
+}
